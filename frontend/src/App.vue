c569281f5e7916f5ce7b74f4869a1ba1b210f2d2
ninspyth <amandavu11@gmail.com>
2024-06-10 18:46:32 +0530

    added navigation bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <div id="app">
+    <div id="logo">
+      <li>Logo</li>
+    </div>
+    <AppNavbar/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppNavbar from './components/AppNavbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppNavbar
   }
-}
+};
 </script>
 
 <style>
+#app>#logo {
+  margin: 15px 0px 0px 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  float: left;
+}
+
+#logo>li {
+  font-size: 20px;
+  list-style-type: none; 
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 1365px;
+  margin: -10px 0px 0px -7px;
+  background-color: grey;
 }
 </style>
---
+++ frontend/src/components/AppNavbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="nav-bar">
+    <ul id="nav-items">
+      <li v-for="link in links" :key="link">
+        <a href="#">{{ link }}</a>
+      </li>
+    </ul>  
+  </nav>
+</template>
+
+<script>
+  export default {
+      name: 'AppNavbar',
+      data() {
+        return {
+          links: ["Home", "Dashboard", "Login", "Register"]
+        }
+      }
+  };
+</script>
+
+<style scoped>
+.nav-bar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#nav-items {
+  display: inherit;
+  justify-content: inherit;
+  align-items: inherit;
+  padding: 0px;
+  
+}
+
+li {
+  font-size: 20px;
+  list-style-type:none;
+  padding: 0px 10px 0px 10px;
+}
+
+#nav-items>a {
+  text-decoration: none;
+}
+</style>
